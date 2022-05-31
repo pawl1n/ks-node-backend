caed2458dda252645a12183bb8a102a6c7f0f1d3
pawl1n <pinchuk.andryusha310212@gmail.com>
2022-05-31 14:21:06 +0300

    fix: removed unique parameter for name

--- models/Product.js
+++ models/Product.js
@@ -4,8 +4,7 @@ const Schema = mongoose.Schema
 const productSchema = new Schema({
 	name: {
 		type: String,
-		required: true,
-		unique: true
+		required: true
 	},
 	description: {
 		type: String,
