626c4d7bbdac88a376d0fb0a755ad797e270d402
pawl1n <pinchuk.andryusha310212@gmail.com>
2022-06-06 22:32:09 +0300

    fix

--- models/User.mjs
+++ models/User.mjs
@@ -1,51 +1,49 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	password: {
-		type: String,
-		required: true
-	},
-	phone: {
-		type: String,
-		required: true
-	},
-	isAdmin: {
-		type: Boolean,
-		default: false
-	},
-	shipping: {
-		city: {
-			type: String,
-			default: ''
-		},
-		zip: {
-			type: String,
-			default: ''
-		},
-		street: {
-			type: String,
-			default: ''
-		},
-		appartament: {
-			type: String,
-			default: ''
-		},
-		department: {
-			type: String
-		},
-		shippingMethod: {
-			type: String
-		}
-	}
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  shipping: {
+    city: {
+      type: String,
+      default: ''
+    },
+    zip: {
+      type: String,
+      default: ''
+    },
+    street: {
+      type: String,
+      default: ''
+    },
+    appartament: {
+      type: String,
+      default: ''
+    },
+    department: {
+      type: String
+    },
+    shippingMethod: {
+      type: String
+    }
+  }
 })
 
 export default mongoose.model('User', userSchema)
--- routes/users.mjs
+++ routes/users.mjs
@@ -1,18 +1,20 @@
 import { Router } from 'express'
 import {
-	login,
-	getAll,
-	getById,
-	create,
-	update,
-	remove
+  login,
+  getAll,
+  getById,
+  create,
+  update,
+  remove,
+  register
 } from '../controllers/users.mjs'
 import passport from 'passport'
 const router = Router()
 
 router.post('/login', login)
-router.get('/', getAll)
-router.get('/:id', getById)
+router.post('/register', register)
+router.get('/', passport.authenticate('jwt', { session: false }), getAll)
+router.get('/:id', passport.authenticate('jwt', { session: false }), getById)
 router.post('/', passport.authenticate('jwt', { session: false }), create)
 router.patch('/:id', passport.authenticate('jwt', { session: false }), update)
 router.delete('/:id', passport.authenticate('jwt', { session: false }), remove)
