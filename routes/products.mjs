385a2083c99a344a92efce3a0dd7f79de81d093a
pawl1n <pinchuk.andryusha310212@gmail.com>
2022-06-07 23:12:02 +0300

    removed unused method

--- routes/products.mjs
+++ routes/products.mjs
@@ -1,19 +1,17 @@
 import { Router } from 'express'
 import {
-	getAll,
-	getById,
-	getByCategoryId,
-	count,
-	create,
-	update,
-	remove
+  getAll,
+  getById,
+  count,
+  create,
+  update,
+  remove
 } from '../controllers/products.mjs'
 import passport from 'passport'
 const router = Router()
 
 router.get('/', getAll)
 router.get('/:id', getById)
-router.get('/get/ByCategory/:id', getByCategoryId)
 router.get('/get/count/', count)
 router.post('/', passport.authenticate('jwt', { session: false }), create)
 router.patch('/:id', passport.authenticate('jwt', { session: false }), update)
